0aa29dc4d4504e53be45631dc67c8e0241e263f7
Nadia <nadiavpicon@gmail.com>
2022-08-06 14:56:03 -0500

    Add files via upload

---
+++ build/mapa.js
@@ -0,0 +1,4 @@
+let mapa = L.map('map').setView([4.639386, -74.082412],10)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
